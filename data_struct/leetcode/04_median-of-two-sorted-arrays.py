a08efbb56345d7c21a165f9e7af7f0e50cef04ae
wujinzhen <jinzhen.wu@tenclass.com>
2023-12-04 20:05:53 +0800

    leetcode_day3

--- data_struct/leetcode/04_median-of-two-sorted-arrays.py
+++ data_struct/leetcode/04_median-of-two-sorted-arrays.py
@@ -10,9 +10,44 @@ from _tools import get_random_str
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        pass
+        """
+        https://algo.itcharge.cn/Solutions/0001-0099/median-of-two-sorted-arrays/#%E6%80%9D%E8%B7%AF-1-%E4%BA%8C%E5%88%86%E6%9F%A5%E6%89%BE
 
+        两个正序数组满足以下条件就可以确定中位数, 在|两边就能找到
+        nums1[1:m1], nums2[1:m2] | nums1[m1:], nums2[m2:]
+
+        因为是排好序的数组, 符合O(log (m+n))时间复杂度只能用二分法, 找到这个中分的位置
+        """
+        n1 = len(nums1)
+        n2 = len(nums2)
+        if n1 > n2:  # 选择数量少的数组作为二分的数组,减少二分次数
+            return self.findMedianSortedArrays(nums2, nums1)
+
+        k = (n1 + n2 + 1) // 2  # 代表在两个数组中中位数所处的个数位置
+        left = 0  # 二分数组的左右边界
+        right = n1
+        while left < right:
+            m1 = left + (right - left) // 2  # 数组1的二分点
+            m2 = k - m1  # 数组2的二分点
+            if nums1[m1] < nums2[m2-1]:  # 如果数组1小于数组2说明当前的二分点左边的个数小于K个, 要继续往右移
+                left = m1 + 1
+            else:
+                right = m1
+        # 最终得出的二分点
+        m1 = left
+        m2 = k - m1
+
+        # 如果两个数组的总个数为奇数, 则取二分点左边的最大数
+        c1 = max(float('-inf') if m1 <= 0 else nums1[m1 - 1], float('-inf') if m2 <= 0 else nums2[m2 - 1])
+        if (n1 + n2) % 2 == 1:
+            return c1
+        # 如果总个数为偶数, 则取二分点右边的最大数
+        c2 = min(float('inf') if m1 >= n1 else nums1[m1], float('inf') if m2 >= n2 else nums2[m2])
+        return (c1 + c2) / 2
 
 
 if __name__ == '__main__':
-    pass
+    nums1 = [-1, 0, 4, 9, 10]
+    nums2 = [2, 3, 5, 19, 30, 98]
+    res = Solution().findMedianSortedArrays(nums1, nums2)
+    print(res)
---
+++ data_struct/leetcode/05_longest-palindromic-substring.py
@@ -0,0 +1,46 @@
+"""
+给你一个字符串 s，找到 s 中最长的回文子串。
+如果字符串的反序与原始字符串相同，则该字符串称为回文字符串。
+"""
+import copy
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        s_len = len(s)
+        if s_len < 2:
+            return s
+
+        max_len = 1  # 最长回文长度
+        begin = 0  # 最长回文开始位置
+        # 二维列表,用来存储i到j之间知否是回文, dp[i][j] = True表示i到j之间是回文
+        dp = [[False]*s_len for i in range(s_len)]
+        # 字符长度为1都是回文
+        for i in range(s_len):
+            dp[i][i] = True
+        # # 先枚举回文子串的长度, 然后根据左边界和长度确定右边界
+        for L in range(2, s_len+1):
+            # 确定左边界
+            for i in range(s_len):
+                # 确定右边界,由 L 和 i 可以确定右边界，即 j - i + 1 = L 得
+                j = L + i - 1
+                # 如果右边界超过则结束
+                if j >= s_len:
+                    break
+                if s[i] == s[j]:
+                    # 如果字符串长度超过3则需要看中间的子串是否是回文
+                    if j - i < 3:
+                        dp[i][j] = True
+                    else:
+                        dp[i][j] = dp[i+1][j-1]
+                else:
+                    dp[i][j] = False
+                if dp[i][j] and j - i + 1 > max_len:
+                    max_len = j - i + 1
+                    begin = i
+        return s[begin:begin+max_len]
+
+
+if __name__ == '__main__':
+    s = '1234545432'
+    res = Solution().longestPalindrome(s)
+    print(res)
